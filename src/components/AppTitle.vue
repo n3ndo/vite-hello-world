1d149f0f45c5dae5fb79aea04a26aae2965db0a2
n3ndo <ferdinandof3lixbagnale@gmail.com>
2023-12-21 18:42:08 +0100

    [UPDATE] exercise done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!',
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+<style lang="">
+    
+</style>
